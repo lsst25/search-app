9e5ce5f0cb834e57e4992e58706ed5641eb63cf0
lsst25 <lsst25@gmail.com>
2022-12-12 22:46:07 +0200

    chore: install prettier

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  SEARCH_API_URL: '/proxy',
+  SEARCH_API_KEY: '468ace02e951a72eea71258183f0a194d59deb2837f04da7c7fc2c5e3b11a545'
 };
