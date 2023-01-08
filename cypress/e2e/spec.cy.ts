4e119eb38430d7bd2e87fcd52ff749976572eeed
lsst25 <lsst25@gmail.com>
2023-01-08 19:22:49 +0200

    test: add more checks to first spec

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -2,5 +2,7 @@ describe('My First Test', () => {
   it('Visits the initial project page', () => {
     cy.visit('/')
     cy.contains('Search App')
+    cy.contains('Home')
+    cy.contains('News')
   })
 })
